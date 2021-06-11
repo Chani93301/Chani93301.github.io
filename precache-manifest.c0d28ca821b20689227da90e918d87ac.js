f38d0a5bd760a5a887603becf0c3212abd9cb836
Chani93301 <93301@fhss.sjp.ac.lk>
2021-06-11 19:04:26 +0530

    Updates

---
+++ precache-manifest.c0d28ca821b20689227da90e918d87ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0671c6856c59e2b80038847729058d93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2aad86fb507b5ca07ac",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "2c652c68bd4f83e02285",
+    "url": "/static/js/2.9f12b638.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.9f12b638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2aad86fb507b5ca07ac",
+    "url": "/static/js/main.f74f4662.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "b45df37df393198806d16a101a5b7987",
+    "url": "/static/media/profileImage.b45df37d.png"
+  },
+  {
+    "revision": "d52d32e954027323e344474aaba9fa44",
+    "url": "/static/media/resume.d52d32e9.pdf"
+  }
+]);
